054d15f817b976747e8b11af0ac84856abb70325
youngniw <1999julie@naver.com>
2022-04-14 22:04:01 +0900

    initial project

---
+++ src/main/java/com/nadoyagsa/pillaroid/PillaroidApplication.java
@@ -0,0 +1,13 @@
+package com.nadoyagsa.pillaroid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillaroidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PillaroidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nadoyagsa/pillaroid/PillaroidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nadoyagsa.pillaroid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillaroidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
